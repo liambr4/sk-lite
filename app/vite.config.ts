d3ded9901bdcc3e6c037bf5bd76f570cb91adaa1
Liam Brown <liam.brown@dal.ca>
2024-06-13 23:05:36 -0300

    unused vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,16 +3,5 @@ import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/",
   plugins: [preact()],
-  // preview: {
-  //   port: 8080,
-  //   strictPort: true,
-  // },
-  // server: {
-  //   port: 5173,
-  //   strictPort: true,
-  //   host: true,
-  //   origin: "http://0.0.0.0:8080",
-  // },
 });
